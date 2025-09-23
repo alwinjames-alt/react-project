c3c3f1c4884c6ae897620f5a1a5a8761f9b2172f
Alwin James <alwin.james@thebluespire.com>
2025-09-23 19:48:19 +0530

    add .gitignore and update posts file path in data handling

--- dummy-backend/data/posts.js
+++ dummy-backend/data/posts.js
@@ -1,14 +1,18 @@
 const fs = require('node:fs/promises');
+const path = require('path');
+
+// Full path to posts.json in the same folder as this script
+const postsFilePath = path.join(__dirname, 'posts.json');
 
 async function getStoredPosts() {
-  const rawFileContent = await fs.readFile('posts.json', { encoding: 'utf-8' });
+  const rawFileContent = await fs.readFile(postsFilePath, { encoding: 'utf-8' });
   const data = JSON.parse(rawFileContent);
   const storedPosts = data.posts ?? [];
   return storedPosts;
 }
 
 function storePosts(posts) {
-  return fs.writeFile('posts.json', JSON.stringify({ posts: posts || [] }));
+  return fs.writeFile(postsFilePath, JSON.stringify({ posts: posts || [] }));
 }
 
 exports.getStoredPosts = getStoredPosts;
